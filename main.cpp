228f043f6fdf7f514b09eb01c6fb87a5cf3565ff
hundal78 <hundal.ias78@gmail.com>
2014-10-21 19:30:52 -0700

    last shot

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 void initializer(char ** ptr);
 
+// this function takes in a string and returns it as tokens in the form of a
+// char **, it also reutrns the number of arguments including the connectors
 void parser(string inp, char ** args, int& count)
 {
    count=0; 
@@ -25,6 +27,17 @@ void parser(string inp, char ** args, int& count)
 			if(inp.at(i)==' ')
 			{
 				i++;
+				break;
+			}
+			else if(inp.at(i)=='#')
+			{
+				if(s.size()>0)
+				{
+					args[count]=new char[s.size()+1];
+					strcpy(args[count],s.c_str());
+					count++;
+				}
+				i=inp.size();
 				break;
 			}
 			else if(inp.at(i)==';')
@@ -137,11 +150,12 @@ void shell()
    string input;
 	while(1)
 	{
-		cout << endl << "rshell: " << flush;
-		getline(cin, input);
 		int pid=fork();
+		cout << "PId 1 is " << pid << endl;
 		if(0==pid)
 		{
+			cout << "rshell: " << flush;
+			getline(cin, input);
 			char * args[30];
 			initializer(args);
 			int targs=0;
@@ -152,22 +166,47 @@ void shell()
 			{
 				char * curargs[30];
 				breaker(args,curargs,index,stopstat);
+				cout << "curr stopstatus is " << stopstat << endl;
+				for(int j=0; args[j]!=0;j++)
+				{
+					cout << curargs[j] << " " << j  <<  endl;
+				}
+				cout << endl;
+				cout << "pre check"<< endl;
 				int pid2=fork();
+				cout << "pid 2 " <<  pid2 << endl;
 				if(0==pid2)
 				{
-					execvp(curargs[0], curargs);
-					exit(1);
+					cout << "I am child" << endl;
+					if(execvp(curargs[0], curargs)==-1)
+					perror("Execution failed");
+					exit(0);
 				}
 				else if(pid2>0)
 				{
-					wait(NULL);
+					cout << "Tester" << endl;
+					int *status=new int;
+					*status=-1;
+					if(wait(status)==-1) perror("Wait failed");
+				   cout << "I am parent" << endl;
+					cout << "Status is: " << *status << endl;
+					if(*status!=0)
+					{
+						cout << "Error: invalid command" << endl;
+						if(stopstat==2) break;
+					}
+					else
+					{
+						if(stopstat==3) break;
+					}
 				}
 			}
-			exit(1);
+			exit(0);
 		}
 		else if(pid>0)
 		{
 			wait(NULL);
+			continue;
 		}
 		else
 		{
@@ -187,27 +226,47 @@ void initializer(char ** ptr)
 int main()
 {
 	shell();
-	/*string input;
+/*	string input;
 	char* args[30];
 	initializer(args);
 	int targs=0;
 	cout << "Input: " << flush;
 	getline(cin, input);
 	parser(input, args, targs);
+	for(int ii=0;args[ii]!=0;ii++)
+	{
+		cout << args[ii] << " " << flush; 
+	}
+	cout << "Check 1 " << endl;
 	int stopstat=-1;
 	int index=0;
 	while(stopstat!=0)
 	{
 		char * curargs[30];
 		breaker(args,curargs,index,stopstat);
-		for(int j=0; curargs[j]!=0;j++)
+		for(int j=0; args[j]!=0;j++)
 		{
 			cout << curargs[j] << " " << flush;
 		}
 		cout << endl;
-	}
-	//int x=execvp(args[0],args);
-	//cout << x << endl;*/
+		int pid4=fork();
+		if(0==pid4)
+		{
+			cout << "I am child" << endl;
+			int x=execvp(curargs[0], curargs);
+			cout << "Error" << endl;
+			exit(0);
+
+		}
+		else if(pid4>0)
+		{
+			int *status=new int;
+			*status=-1;
+			if(wait(status)==-1) perror("Wait failed");
+			cout << "Status is: " << *status << endl;
+			if(*status!=0) cout << "Error: invalid command" << endl;
+		}
+	}*/
 	
 	return 0;
 }

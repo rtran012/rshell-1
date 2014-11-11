f472abe4d8b137173f9e452758a6460c9dcf2e80
hundal78 <hundal.ias78@gmail.com>
2014-11-11 10:53:10 -0800

    breakfast break

--- src/ls.cpp
+++ src/ls.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <errno.h>
@@ -18,7 +19,7 @@ void order(vector<const char*> &list)
 		min=i;
 		for(j=i+1;j<list.size();j++)
 		{
-			if(strcmp(list.at(j),list.at(min))<0) min=j;
+			if(strcasecmp(list.at(j),list.at(min))<0) min=j;
 		}
 		//swap
 		if(min!=i)
@@ -56,8 +57,11 @@ void listmaker(vector<const char*> &nl, vector<bool> &exec, vector<bool> &dir)
   if(closedir(currdir)==-1) perror("Closedir");
 }
 
-void normal(bool hid)
+void normal(bool showhid)
 {
+	struct winsize w;
+	int used=0;
+	if(ioctl(0,TIOCGWINSZ,&w)==-1) perror("ioctl");
 	vector<const char*> nl;
 	vector<bool> exec,dir;
 	listmaker(nl,exec,dir);
@@ -67,19 +71,78 @@ void normal(bool hid)
     if(dir.at(i)) cout << "\x1b[34m";
     if(nl.at(i)[0]=='.')
     {
-      if(hid)
+      if(showhid)
       {
         cout << "\x1b[40;1m";
-        cout << nl.at(i) << endl;
-      }
+				if((w.ws_col-used)<strlen(nl.at(i)))
+				{
+					cout << endl << nl.at(i) << "\x1b[0m" << "  ";	
+					used=0;
+				}
+				else
+				{
+					cout << nl.at(i) << "\x1b[0m" << "  ";
+					used+=(strlen(nl.at(i))+2);
+				}
+			}
     }
-    else cout << nl.at(i) << endl;
-    cout << "\x1b[0m";
+    else
+		{
+			if((w.ws_col-used)<strlen(nl.at(i)))
+			{
+				cout << endl << nl.at(i) << "\x1b[0m" << "  ";	
+				used=0;
+			}
+			else
+			{
+				cout << nl.at(i) << "\x1b[0m" << "  ";
+				used+=(strlen(nl.at(i))+2);
+			}
+		}	
+		cout << "\x1b[0m";
   }
+	cout << endl;
 }
 
+void minilistmaker(vector<const char*> &nl)
+{
+  DIR* currdir=opendir(".");
+  if(currdir==NULL) perror("Opendir");
+  dirent * curdiren=readdir(currdir);
+  if(curdiren ==0 && errno !=0) perror("read"); 
+  while(curdiren!=0)
+  { 
+		nl.push_back(curdiren->d_name);	
+    curdiren=readdir(currdir);
+    if(curdiren ==0 && errno !=0) perror("read"); 
+  }
+	order(nl); 
+	if(closedir(currdir)==-1) perror("Closedir");
+}
+
+void dprinter(const char * path)
+{
+	struct stat outbuf;
+  int x=stat(path,&outbuf);
+  if(x==-1) perror("Stat");
+    if((outbuf.st_mode)&S_IFDIR) dir.push_back(true);
+    else dir.push_back(false);
+    if((outbuf.st_mode)&S_IXUSR) exec.push_back(true);
+    else exec.push_back(false);
+}
+
+void detailed(bool showhid)
+{
+	vector<const char*> nl;
+	minilistmaker(nl);
+  for(int i=0;i<nl.size();i++)
+  {
+		dprinter(nl.at(i));
+	}
+}
 int main()
 {
-	//normal(1);
+	//normal(0);
+	
 	return 0;
 }

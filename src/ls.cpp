d54b2eb86f29a910873856b7b48e5018be269104
hundal78 <hundal.ias78@gmail.com>
2014-11-11 14:25:39 -0800

    experiment

--- src/ls.cpp
+++ src/ls.cpp
@@ -9,9 +9,12 @@
 #include <errno.h>
 #include <stdio.h>
 #include <vector>
+#include <grp.h>
+#include <pwd.h>
+#include <time.h>
 using namespace std;
 
-void order(vector<const char*> &list)
+void order(vector<char*> &list)
 {
 	int i,j,min;
 	for(i=0;i<list.size();i++)
@@ -24,14 +27,14 @@ void order(vector<const char*> &list)
 		//swap
 		if(min!=i)
 		{
-			const char * temp=list.at(i);
+			char * temp=list.at(i);
 			list.at(i)=list.at(min);
 			list.at(min)=temp;
 		}
 	}
 }
 
-void listmaker(vector<const char*> &nl, vector<bool> &exec, vector<bool> &dir)
+void listmaker(vector<char*> &nl, vector<bool> &exec, vector<bool> &dir)
 {
 	struct stat outbuf;
   DIR* currdir=opendir(".");
@@ -40,7 +43,9 @@ void listmaker(vector<const char*> &nl, vector<bool> &exec, vector<bool> &dir)
   if(curdiren ==0 && errno !=0) perror("read"); 
   while(curdiren!=0)
   { 
-		nl.push_back(curdiren->d_name);	
+		nl.push_back(0);	
+		nl.back()=new char[strlen(curdiren->d_name)+1];
+		strcpy(nl.back(),curdiren->d_name);
     curdiren=readdir(currdir);
     if(curdiren ==0 && errno !=0) perror("read"); 
   }
@@ -62,7 +67,7 @@ void normal(bool showhid)
 	struct winsize w;
 	int used=0;
 	if(ioctl(0,TIOCGWINSZ,&w)==-1) perror("ioctl");
-	vector<const char*> nl;
+	vector<char*> nl;
 	vector<bool> exec,dir;
 	listmaker(nl,exec,dir);
   for(int i=0;i<nl.size();i++)
@@ -104,7 +109,7 @@ void normal(bool showhid)
 	cout << endl;
 }
 
-void minilistmaker(vector<const char*> &nl)
+void minilistmaker(vector<char*> &nl)
 {
   DIR* currdir=opendir(".");
   if(currdir==NULL) perror("Opendir");
@@ -112,7 +117,9 @@ void minilistmaker(vector<const char*> &nl)
   if(curdiren ==0 && errno !=0) perror("read"); 
   while(curdiren!=0)
   { 
-		nl.push_back(curdiren->d_name);	
+		nl.push_back(0);	
+		nl.back()=new char[strlen(curdiren->d_name)+1];
+		strcpy(nl.back(),curdiren->d_name);
     curdiren=readdir(currdir);
     if(curdiren ==0 && errno !=0) perror("read"); 
   }
@@ -120,29 +127,83 @@ void minilistmaker(vector<const char*> &nl)
 	if(closedir(currdir)==-1) perror("Closedir");
 }
 
-void dprinter(const char * path)
+void dprinter(char * path)
 {
 	struct stat outbuf;
-  int x=stat(path,&outbuf);
+  int x=lstat(path,&outbuf);
   if(x==-1) perror("Stat");
-    if((outbuf.st_mode)&S_IFDIR) dir.push_back(true);
-    else dir.push_back(false);
-    if((outbuf.st_mode)&S_IXUSR) exec.push_back(true);
-    else exec.push_back(false);
+  
+//	cout << "dir: " << S_ISDIR(outbuf.st_mode) << " link: " << S_ISLNK(outbuf.st_mode) << " reg: "<< S_ISREG(outbuf.st_mode) <<  endl;
+	if(S_ISDIR(outbuf.st_mode)) cout << 'd';
+  else if(S_ISLNK(outbuf.st_mode)) cout << 'l';
+	else cout << '-';
+  
+	if((outbuf.st_mode)&S_IRUSR) cout << 'r';
+	else cout << '-';
+	if((outbuf.st_mode)&S_IWUSR) cout << 'w';
+	else cout << '-';
+	if((outbuf.st_mode)&S_IXUSR) cout << 'x';
+	else cout << '-';
+
+	if((outbuf.st_mode)&S_IRGRP) cout << 'r';
+	else cout << '-';
+	if((outbuf.st_mode)&S_IWGRP) cout << 'w';
+	else cout << '-';
+	if((outbuf.st_mode)&S_IXGRP) cout << 'x';
+	else cout << '-';
+
+	if((outbuf.st_mode)&S_IROTH) cout << 'r';
+	else cout << '-';
+	if((outbuf.st_mode)&S_IWOTH) cout << 'w';
+	else cout << '-';
+	if((outbuf.st_mode)&S_IXOTH) cout << 'x';
+	else cout << '-';
+
+	cout << " ";
+	cout << outbuf.st_nlink;
+
+	cout << " ";
+	struct passwd * pwd=getpwuid(outbuf.st_uid);
+	cout << pwd->pw_name;
+
+	cout << " ";
+	struct group * grp=getgrgid(outbuf.st_gid);
+	cout << grp->gr_name;
+	
+	cout << " ";
+	cout << outbuf.st_size;
+
+	cout << " ";
+	struct tm *at=localtime(&outbuf.st_mtime);
+	cout << at->tm_mon << '/' << at->tm_mday << '/' << at->tm_year+1900;
+	cout << " " << at->tm_hour << ":" << at->tm_min;
+
+	cout << " ";
+	if((outbuf.st_mode)&S_IXUSR) cout << "\x1b[32;1m";
+  if((outbuf.st_mode)&S_IFDIR) cout << "\x1b[34m";
+	if(S_ISLNK(outbuf.st_mode))	cout << "\x1b[36;1m";
+  if(path[0]=='.') cout << "\x1b[40;1m";
+	cout << path;	
+	cout << "\x1b[0m";
+	cout << endl;
 }
 
 void detailed(bool showhid)
 {
-	vector<const char*> nl;
+	vector<char*> nl;
 	minilistmaker(nl);
   for(int i=0;i<nl.size();i++)
   {
-		dprinter(nl.at(i));
+		if(nl.at(i)[0]=='.')
+		{
+			if(showhid) dprinter(nl.at(i));
+		}
+		else dprinter(nl.at(i));
 	}
 }
 int main()
 {
-	//normal(0);
-	
+	//normal(1);
+	detailed(1);
 	return 0;
 }
